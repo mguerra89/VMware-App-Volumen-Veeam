c5467c4c13fb8df8bb3d4d44864f677e2d1c72b3
mguerra89 <71152467+mguerra89@users.noreply.github.com>
2023-12-29 09:42:09 -0500

    Add files via upload

---
+++ Mount_disks.ps1
@@ -0,0 +1,68 @@
+﻿#Script tested on VMware App Volumes 4.0 and Veeam V12
+# You should run this lines at least once
+#Set-PowerCLIConfiguration -Scope User -ParticipateInCeip $true -Confirm:$false
+#Set-PowerCLIConfiguration -InvalidCertificateAction Ignore -Confirm:$false
+
+#vCenter URL and Credentials to establish connection
+
+$vCenterServer= "vcenter.vmware.com" #vCenter FQDN
+$User = "user@domain.com"
+$Passwd = "******"
+
+# Name of the Datastore on which vmdk files are located & root path on which Writable Volumes VMDKs are located
+$datastoreName = "STORAGE_NAME"
+$rootPath = "[STORAGE_NAME]/appvolumes/writables_backup"
+
+# Name of the VM which disks will be mounted on
+$VMName = "DummyVM_NAME"
+
+
+#Script Start
+
+
+#Establish vCenter Server Instance Connection
+Connect-VIServer -Server $vCenterServer -User $User -Password $Passwd
+
+# Obtain Datastore Data
+$datastore = Get-Datastore -Name $datastoreName
+
+# Obtain VM Data
+$vm = Get-VM -Name $VMName
+Write-Host "VM: " $vm.Name
+
+
+$strDatastoreName = $datastore.name
+$ds = Get-Datastore -Name $strDatastoreName | %{Get-View $_.Id}
+$fileQueryFlags = New-Object VMware.Vim.FileQueryFlags
+$fileQueryFlags.FileSize = $true
+$fileQueryFlags.FileType = $true
+$fileQueryFlags.Modification = $true
+$searchSpec = New-Object VMware.Vim.HostDatastoreBrowserSearchSpec
+$searchSpec.details = $fileQueryFlags
+$searchSpec.sortFoldersFirst = $true
+$dsBrowser = Get-View $ds.browser
+
+Write-Host "Searching path: "$rootPath `n
+$searchResult = $dsBrowser.SearchDatastoreSubFolders($rootPath, $searchSpec)
+
+foreach ($folder in $searchResult) {
+            foreach ($fileResult in $folder.File) {
+                $file = "" | Select-Object Name, FullPath   
+                $file.Name = $fileResult.Path
+                $strFilename = $file.Name
+                #Check that the vmdk is not a snapshot or a Writable Volumes metadata file.
+                if ($strFilename -and $strFilename.Contains(".vmdk") -and !$strFilename.Contains(".metadata") -and  !$strFilename.Contains("-flat.vmdk") -and !$strFilename.Contains("delta.vmdk")) {
+                    $folderpath = $folder.FolderPath
+                    $strFullPath = $folderpath + $strFilename
+                    Write-Host "File Path is: " $strFullPath
+                    New-HardDisk -VM $vm -DiskPath $strFullPath | Out-Null
+                    Write-Host "Disk mounted" `n -ForegroundColor Green
+                    }
+                }
+
+            }
+
+Write-Host "Task finished" -ForegroundColor Yellow
+
+# Disconnect from vCenter server Instance
+Disconnect-VIServer -Confirm:$false
---
+++ Unmount _Disks.ps1
@@ -0,0 +1,33 @@
+﻿#Script tested on VMware App Volumes 4.0 and Veeam V12
+# You should run this lines at least once
+#Set-PowerCLIConfiguration -Scope User -ParticipateInCeip $true -Confirm:$false
+#Set-PowerCLIConfiguration -InvalidCertificateAction Ignore -Confirm:$false
+
+#vCenter URL and Credentials to establish connection
+
+$vCenterServer= "vcenter.vmware.com" #vCenter FQDN
+$User = "user@domain.com"
+$Passwd = "******"
+
+Connect-VIServer -Server $vCenterServer -User $User -Password $Passwd
+
+
+# Name of the VM which disks will be mounted on
+$VMName = "DummyVM_Name"
+
+#Script Start
+
+# Obtain VM Data
+$vm = Get-VM -Name $VMName
+Write-Host "VM: " $vm.Name
+
+
+
+# Remove all Disks from VM
+Get-HardDisk -VM $vm | Remove-HardDisk -Confirm:$false
+Write-Host "Disks removed" `n -ForegroundColor Red
+Write-Host "Task finished" -ForegroundColor Yellow
+
+
+# Disconnect from vCenter server Instance
+Disconnect-VIServer -Confirm:$false
